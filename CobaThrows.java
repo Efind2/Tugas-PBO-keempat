00d8efd40ed93f07f4c7b16746d6ccdd31ac04ba
Achmad Afifudin <152256295+Efind2@users.noreply.github.com>
2024-09-14 22:53:34 +0700

    Add files via upload

---
+++ CobaThrows.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanKeempatPBO;
+
+/**
+ *
+ * @author Achmad
+ */
+public class CobaThrows{
+
+    public void untukThrows(int a) throws Exception {
+        int[] array = {0, 1, 2, 3};
+        int index = a;
+        System.out.println("index ke" + a + "adalah" + array[index]);
+
+    }
+}
